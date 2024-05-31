a0ef741f3cb03b88353fd50902a21a6a8bccac32
Saidov Zohid <116626702+SaidovZohid@users.noreply.github.com>
2024-06-01 00:15:38 +0200

    postgres up and down done and connection success

---
+++ storage/storage.go
@@ -0,0 +1,15 @@
+package storage
+
+import "github.com/jmoiron/sqlx"
+
+type StorageI interface {
+}
+
+type StoragePg struct {
+}
+
+func NewStorage(psqlConn *sqlx.DB) StorageI {
+	return &StoragePg{
+
+	}
+}
